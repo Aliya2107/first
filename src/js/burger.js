4ffce477e3c395050fd814999434469189327f0b
Baktybekova Aliya <Aliya@Aliya-MacBook-Pro.local>
2021-04-18 13:04:46 +0600

    dobavila index

---
+++ src/js/burger.js
@@ -0,0 +1,4 @@
+document.querySelector('.burger').addEventListener('click', function () {
+    document.querySelector('.burger').classList.toggle('active');
+    document.querySelector('.nav__menu').classList.toggle('active');
+ });
